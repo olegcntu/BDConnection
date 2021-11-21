2bca79ae23da04898cb25cb083dca72df350711b
olegcntu <oleg.krasavcik.41@gmail.com>
2021-11-21 13:10:26 +0200

    all methods work

--- src/main/java/Update.java
+++ src/main/java/Update.java
@@ -1,11 +1,9 @@
 import java.sql.*;
 
-// String st = "INSERT INTO bank (id,town) VALUES(17,'Chernigiv')";
-
 public class Update {
     public static String update(String inquiry) {
         Connection connection = null;
-        String result = null;
+        String result;
         try {
             BDProcessor bdProcessor = new BDProcessor();
             connection = bdProcessor.getConnection("jdbc:postgresql://localhost:5432/Bank", "postgres", "123re");
